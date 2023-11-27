ba17b89768a01585a981329e9fa7ffd2175ddc24
Lloyds LBG Laptop <rick.miles85@gmail.com>
2023-11-27 14:28:54 +0000

    Football Team Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Trainer from './Components/Trainer';
 import Trainee from './Components/Trainee';
+import Football_Team from './Components/Football_Team';
 
 // react component called APP
 
@@ -13,6 +14,7 @@ function App() {
       <p>This is a test webpage to show the features and benefit of React JS</p>
         <Trainer />
       <Trainee />
+      <Football_Team />
          </div >
   );
 }
---
+++ src/components/Football_Team.jsx
@@ -0,0 +1,13 @@
+// Compentn names MUST be capitalised
+function Football_Team() {
+
+    return (
+        <div>
+        <p>Team Name: Sheffield United</p>
+        <p>Founded: 1889</p>
+        <p>League: Premiership</p>
+        </div>
+);
+}
+// lets you import the function into another file
+export default Football_Team;
