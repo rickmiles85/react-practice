3203ae0ee1995f532f37e252fda7c00e0d9b4803
Lloyds LBG Laptop <rick.miles85@gmail.com>
2023-11-27 13:40:31 +0000

    Git Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+// react component called APP
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Testing React JS for the First Time</h3>
+        <body>
+          <form>
+            <p>
+              Hello This is a Test
+            </p>
+            <label>
+              First Name:
+              <input type="text" name="firstName" />
+            </label>
+          </form>
+          <form>
+            <label>
+              Surname:
+              <input type="text" name="lastName" />
+            </label>
+          </form>
+          <form>
+            <label>
+              Place of Birth:
+              <input type="text" name="birthCity" />
+            </label>
+            <input type="Submit" value="Submit" />
+          </form>
+          <table>
+            <tr>
+              <tr>
+                <th>Name</th>
+              </tr>
+              <tr>
+                <td>Rick</td>
+              </tr>
+              <tr>
+                <td>John</td>
+              </tr>
+              <tr>
+                <td>Andrew</td>
+              </tr>
+              <tr>
+                <td>Rebecca</td>
+              </tr>
+              <tr>
+                <td>Seb</td>
+              </tr>
+            </tr>
+          </table>
+        </body>
       </header>
-    </div>
+    </div >
   );
 }
 
